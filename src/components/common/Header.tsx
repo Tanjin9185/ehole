f0df7e558cb030f7f6ea3f2a6e3c7251eb6a1dbd
Tanjin Ahamed <tanjin.ahamed123@gmail.com>
2021-10-15 11:46:40 +0600

    banner complete

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,13 +1,24 @@
+import logo from 'assets/images/logo.png';
 import React from 'react';
-import { Container, Nav, Navbar } from 'react-bootstrap';
-import { FiPhoneCall } from 'react-icons/fi';
+import {
+  Button,
+  Container,
+  FormControl,
+  InputGroup,
+  Nav,
+  Navbar,
+} from 'react-bootstrap';
+import { FiPhoneCall, FiShoppingBag, FiUser } from 'react-icons/fi';
 import { FaRegEnvelopeOpen } from 'react-icons/fa';
 import { BsPhone } from 'react-icons/bs';
+import { CgSearchLoading } from 'react-icons/cg';
+import { IoIosNotificationsOutline } from 'react-icons/io';
+import { TiMessage } from 'react-icons/ti';
 
 const Header = () => {
   return (
     <div className="header__component">
-      <div className="top-header">
+      <div className="top-header py-2 bg-light border-bottom">
         <Container className="d-flex align-items-center justify-content-between">
           <ul className="list-unstyled d-flex align-items-center gap-4">
             <li>
@@ -29,13 +40,46 @@ const Header = () => {
           </div>
         </Container>
       </div>
+      {/* middle header */}
+      <div className="middle-header">
+        <Container>
+          <div className="d-flex align-items-cemter gap-5 py-3">
+            <img className="branding" src={logo} alt="" />
+            <InputGroup>
+              <FormControl
+                className="border border-primary"
+                placeholder="Search Here..."
+                aria-label="Recipient's username"
+                aria-describedby="basic-addon2"
+              />
+              <Button variant="primary" id="button-addon2">
+                <CgSearchLoading className="text-width" />
+              </Button>
+            </InputGroup>
+            <ul className="icon-list list-unstyled d-flex gap-3 ">
+              <li>
+                <FiShoppingBag />
+              </li>
+              <li>
+                <IoIosNotificationsOutline />
+              </li>
+              <li>
+                <TiMessage />
+              </li>
+              <li>
+                <FiUser />
+              </li>
+            </ul>
+          </div>
+        </Container>
+      </div>
       <Navbar bg="dark" variant="dark">
         <Container>
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Navbar.Brand href="#home">Categories</Navbar.Brand>
           <Nav className="m-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <Nav.Link href="#home">News feed</Nav.Link>
+            <Nav.Link href="#features">Merchant Zone</Nav.Link>
+            <Nav.Link href="#pricing">Help</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
